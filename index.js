1f6961bd2e45a5f7b6e5d6c8358b5d08cb4737ea
akhfarisfz <akhfarifz6017@gmail.com>
2024-02-20 23:47:18 +0700

    Penyesuaian Router

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const { KaryawanRouter } = require("./modules/karyawan/routers");
 const { JabatanRouter } = require("./modules/jabatan/routers");
 const { PajakRouter } = require("./modules/pajak/routers");
 const { KeluargaRouter } = require("./modules/keluarga/routers");
+const { DepartemenRouter } = require("./modules/departemen/routers");
+const { AsuransiRouter } = require("./modules/asuransi/routers");
+const { GolonganRouter } = require("./modules/golongan/routers");
+const { AbsensiRouter } = require("./modules/absensi/routers");
+const { PenggajianRouter } = require("./modules/penggajian/routers");
 
 
 const app = express();
@@ -21,6 +26,11 @@ LibModuleRegister(app, "karyawan", KaryawanRouter);
 LibModuleRegister(app, "jabatan", JabatanRouter);
 LibModuleRegister(app, "pajak", PajakRouter);
 LibModuleRegister(app, "keluarga", KeluargaRouter);
+LibModuleRegister(app, "departemen", DepartemenRouter);
+LibModuleRegister(app, "asuransi", AsuransiRouter);
+LibModuleRegister(app, "golongan", GolonganRouter);
+LibModuleRegister(app, "absensi", AbsensiRouter);
+LibModuleRegister(app, "penggajian", PenggajianRouter);
 
 app.listen(process.env.APP_PORT, function () {
   console.log(`Server berjalan di port ${process.env.APP_PORT}.`);
