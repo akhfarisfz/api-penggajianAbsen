c86a6d3c50fe5d6f5254c0c9e4f164d9cba25b95
Seprendiansyah <yseprendiansyahmitra@gmail.com>
2024-02-28 17:57:01 +0700

    midd

--- modules/karyawan/middlewares.js
+++ modules/karyawan/middlewares.js
@@ -94,9 +94,10 @@ const KaryawanMiddlewareUpdate = LibValidationsMiddleware(
   LibValidationFields.CharField({ field: "no_Telepon" }),
   LibValidationFields.CharField({ field: "bank" }),
   LibValidationFields.CharField({ field: "no_rekening" }),
-  LibValidationFields.NumberField({ field: "gajiPokok" }),
   LibValidationFields.ObjectField({ field: "jabatan" }),
-  LibValidationFields.CharField({ field: "jabatanRef" }),
+  LibValidationFields.CharField({ field: "jabatan.nama" }),
+  LibValidationFields.NumberField({ field: "jabatan.gajiPokok" }),
+  LibValidationFields.NumberField({ field: "jabatan.tunjangan" }),
 
   // // LibValidationFields.CharField({ field: "jabatan._id" }),
   // LibValidationFields.CharField({ field: "jabatan.nama" }),
